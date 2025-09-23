1aaa52860eae89a8004bd740be684e9a35b4b8d7
JakubKulesz <132785544+JakubKulesz@users.noreply.github.com>
2025-09-23 17:59:25 +0200

    Update ecm.py

--- ecm.py
+++ ecm.py
@@ -13,7 +13,7 @@ class ECM:
     Attributes
     ----------
     self.model (sm.OLS object): An ecm model.
-    self.longterm_model_ (sm.OLS object): A model for the cointegration vector (longterm dependency).
+    self.longterm_model (sm.OLS object): A model for the cointegration vector (longterm dependency).
 
     Methods
     -------
@@ -28,32 +28,31 @@ class ECM:
 
     def fit(self, X, y):
         data = X.copy()
-        self.longterm_model_ = sm.OLS(y, sm.add_constant(data)).fit()
-        data['coint_vector'] = self.longterm_model_.resid.shift(1)
+        self.longterm_model = sm.OLS(y, sm.add_constant(data)).fit()
+        data['coint_vector'] = self.longterm_model.resid.shift(1)
 
         data['d_y'] = y.diff()
         d_x = [f'd_{col}' for col in X]
         data[d_x] = data[X.columns].diff()
-        data = sm.add_constant(data)
 
-        self.model = sm.OLS(data['d_y'][1:], data[['const', 'coint_vector'] + d_x][1:]).fit()
+        self.model = sm.OLS(data['d_y'][1:], data[['coint_vector'] + d_x][1:]).fit()
 
         return self
     
     def predict(self, X, X_start, y_start):
 
-        X_L = X_start[X_start.index.isin(self.longterm_model_.params.index[1:])]
+        X_L = X_start[X_start.index.isin(self.longterm_model.params.index[1:])]
         y_L = y_start
 
         predictions = [y_start]
         for i in range(0, len(X)):
-            cointegration_vector = y_L - (self.longterm_model_.params['const'] + self.longterm_model_.params.iloc[1:]@X_L)
-            d_X = X.loc[:, self.longterm_model_.params.index[1:]].iloc[i] - X_L
+            cointegration_vector = y_L - (self.longterm_model.params['const'] + self.longterm_model.params.iloc[1:]@X_L)
+            d_X = X.loc[:, self.longterm_model.params.index[1:]].iloc[i] - X_L
             d_X.index = 'd_' + d_X.index.astype(str)
-            d_y = self.model.params['const'] + (self.model.params[d_X.index]@d_X) + (self.model.params['coint_vector'] * cointegration_vector)
+            d_y = (self.model.params[d_X.index]@d_X) + (self.model.params['coint_vector'] * cointegration_vector)
             y_hat = y_L + d_y
             predictions.append(y_hat)
             y_L = y_hat
-            X_L = X.loc[:, self.longterm_model_.params.index[1:]].iloc[i]
+            X_L = X.loc[:, self.longterm_model.params.index[1:]].iloc[i]
 
         return np.array(predictions)
